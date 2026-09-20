a409c64b2385674dfec46b851baecc96077ad679
prdanielcunha <pastordanielpcunha@gmail.com>
2026-09-20 00:39:06 -0300

    refactor(brand): advertise MusicScale Live Node on LAN

--- packages/live-node/src/peerDiscovery.ts
+++ packages/live-node/src/peerDiscovery.ts
@@ -4,7 +4,7 @@ const DEFAULT_GROUP = '239.255.43.17';
 const DEFAULT_PORT = 4318;
 const DEFAULT_BEACON_MS = 3000;
 const DEFAULT_TTL_MS = 12_000;
-const PRODUCT = 'MillionsNest Live Node';
+const PRODUCT = 'MusicScale Live Node';
 const PROTOCOL_VERSION = 1;
 
 export interface DiscoveredLiveNode {
